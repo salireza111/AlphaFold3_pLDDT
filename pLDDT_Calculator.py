45a9c6c3b9b692c3cfe2ff0cb086da2ad322c325
salireza111 <s.alireza_h@yahoo.com>
2024-05-15 19:47:32 +0330

    Update pLDDT_Calculator.py

--- pLDDT_Calculator.py
+++ pLDDT_Calculator.py
@@ -1,16 +1,13 @@
 """
-@author: alireza
-
 place this file on AlphaFold3 predicted directory
-In case of using AlphaFold2 replace ('atom_plddts') with ('plddt') in line 12
+In case of using AlphaFold2 replace ('atom_plddts') with ('plddt') in line 10
 """
-
 import json
 import os
 import numpy as np
+
 #In case of using AlphaFold2 replace ('atom_plddts') with ('plddt') 
 FindFor = 'atom_plddts'
-
 
 def find_json_files(directory):
     json_files = []
@@ -21,7 +18,8 @@ def find_json_files(directory):
                 
     return json_files
 
-files = find_json_files('/Users/alireza/Desktop/Works/Zist Rayanesh/H1129/AF3/fold_h1129-2')
+current_directory = os.path.dirname(os.path.abspath(__file__))
+files = find_json_files(current_directory)
 for file in files:
     with open(file) as f:
         d = json.load(f)
